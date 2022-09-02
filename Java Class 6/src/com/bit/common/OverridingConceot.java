4165329b85374e98e996c28e665c4c39d5ca6c79
sadik41 <sadikurrahman41@gmail.com>
2022-09-01 23:01:55 -0400

    first commit

--- Java Class 6/src/com/bit/common/OverridingConceot.java
+++
@@ -1,8 +0,0 @@
-package com.bit.common;
-
-public class OverridingConceot  {
-
-	// when we do method overriding only thing we can change is the body
-	// we must create is a relation/inherit to do method overriding
-	// method name, access modifier and parameter must be the same
-}
